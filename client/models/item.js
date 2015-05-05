0f56270b74808839e13d587c020f532f08900194
Chris Oyler <christopher.oyler@gmail.com>
2015-05-05 01:01:42 -0700

    needs lipstick, otherwise working

--- client/models/item.js
+++ client/models/item.js
@@ -2,19 +2,20 @@
 
 angular.module('groceries')
 .factory('Item', function($http, nodeUrl){
-  
+
   function Item(){}
-  
-  Item.save = function(item){
+
+  Item.create = function(item){
     return $http.post(nodeUrl + '/items', item);
   };
-  
   Item.getRecords = function(){
     return $http.get(nodeUrl + '/items');
   };
   Item.destroy = function(item){
     return $http.delete(nodeUrl + '/items/' + item._id);
   };
-  
+  Item.update = function(item, itemKey){
+    return $http.put(nodeUrl + '/items/' + itemKey + '/edit', item);
+  };
   return Item;
 });
--- client/views/groceries/groceries.js
+++ client/views/groceries/groceries.js
@@ -5,12 +5,13 @@ angular.module('groceries')
   
   $scope.item = {};
   $scope.camera = false;
-  
+  $scope.editMode = false;
   getRecords();
   
   function getRecords(){
     Item.getRecords()
     .then(function(response){
+      
       $scope.items = response.data.items;
     });
   }
@@ -29,11 +30,14 @@ angular.module('groceries')
       $scope.item.image = dataUrl;
     });
   };
-  $scope.save = function(item){
-    Item.save(item)
+  $scope.create = function(item){
+    Item.create(item)
     .then(function(returnedRecord){
       $scope.item = {};
       $scope.items.push(returnedRecord.data);
+    })
+    .catch(function(){
+      $window.swal({title: 'Profile Error', text: 'There was a problem saving your profile. Please try again.', type: 'error'});
     });
   };
   $scope.destroy = function(item){
@@ -46,13 +50,27 @@ angular.module('groceries')
     });
   };
   $scope.update = function(item){
-    // Item.update(item)
-    // .then(function(returnedRecord){
-    //   //
-    // })
+    var itemKey = item._id;
+    var newItem = angular.copy(item);
+    delete newItem._id;
+    delete newItem.userId;
+    delete newItem.__v;
+    delete newItem.createdAt;
+    Item.update(newItem, itemKey)
+    .then(function(returnedRecord){
+      item = returnedRecord;
+    });
+    $scope.item = {};
+    $scope.editMode = false;
   };
   $scope.edit = function(item){
     $scope.item = item;
+    $scope.editMode = true;
+    
+  };
+  $scope.toggleIsHave = function(item){
+    item.isHave = item.isHave ? true : false;
+    $scope.update(item);
     
   };
   
